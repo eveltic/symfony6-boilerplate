31ab9a8c6850d6441388290c8540cab93c295bee
eveltic <ipforner@gmail.com>
2022-07-23 21:42:31 +0200

    Package tests

---
+++ packages/eveltic/form-upload/src/MyLib.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Eveltic\FormUpload;
+
+class MyLib
+{
+    public static function test()
+    {
+        return 'test233333eee';
+    }
+}
--- src/Controller/Frontend/IndexController.php
+++ src/Controller/Frontend/IndexController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Eveltic\FormUpload\MyLib;
 
 #[Route('/', defaults: [], name: 'app_frontend_index_')]
 class IndexController extends AbstractController
@@ -15,6 +15,7 @@ class IndexController extends AbstractController
     #[Route('/', name: 'index')]
     public function index(Request $request): Response
     {
+        dump(MyLib::test());
         dump($request);
         return new Response('<!DOCTYPE html><html><head></head><body>Frontend index controller</body></html>');
     }
