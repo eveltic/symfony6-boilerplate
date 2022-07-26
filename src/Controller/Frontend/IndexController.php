2071b34df02dabea2c944719ace60aa2d6c36778
eveltic <ipforner@gmail.com>
2022-07-26 21:03:01 +0200

    Added rate limiter to 2fa

--- src/Controller/Frontend/IndexController.php
+++ src/Controller/Frontend/IndexController.php
@@ -3,10 +3,11 @@
 namespace App\Controller\Frontend;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\RateLimiter\RateLimiterFactory;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/', defaults: [], name: 'app_frontend_index_')]
 class IndexController extends AbstractController
@@ -14,7 +15,6 @@ class IndexController extends AbstractController
     #[Route('/', name: 'index')]
     public function index(Request $request): Response
     {
-        dump($request);
         return new Response('<!DOCTYPE html><html><head></head><body>Frontend index controller</body></html>');
     }
 }
--- src/EventSubscriber/SchecbTwoFactorSubscriber.php
+++ src/EventSubscriber/SchecbTwoFactorSubscriber.php
@@ -4,23 +4,48 @@ namespace App\EventSubscriber;
 
 use App\Constants\UserNoticeConstants;
 use App\Manager\UserNoticeManager;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Scheb\TwoFactorBundle\Security\Authentication\Exception\InvalidTwoFactorCodeException;
 use Scheb\TwoFactorBundle\Security\TwoFactor\Event\TwoFactorAuthenticationEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\RateLimiter\RateLimiterFactory;
 
 class SchecbTwoFactorSubscriber implements EventSubscriberInterface
 {
     private UserNoticeManager $userNoticeManager;
+    private RateLimiterFactory $schebTwoFactorLimiter;
 
-    public function __construct(UserNoticeManager $userNoticeManager)
+    public function __construct(UserNoticeManager $userNoticeManager, RateLimiterFactory $schebTwoFactorLimiter)
     {
         $this->userNoticeManager = $userNoticeManager;
+        $this->schebTwoFactorLimiter = $schebTwoFactorLimiter;
     }
 
     public static function getSubscribedEvents(): array
     {
         return [
             TwoFactorAuthenticationEvents::FORM => 'onFormEvent',
+            TwoFactorAuthenticationEvents::FAILURE => 'onAuthenticationFailure',
+            TwoFactorAuthenticationEvents::ATTEMPT => 'onAuthenticationAttempt',
+            TwoFactorAuthenticationEvents::SUCCESS => 'onAuthenticationSuccess',
         ];
+    }
+
+    public function onAuthenticationAttempt($event): void
+    {
+        $oLimiter = $this->schebTwoFactorLimiter->create($event->getRequest()->getClientIp());
+        if (!$oLimiter->consume(1)->isAccepted()) {
+            throw new InvalidTwoFactorCodeException();
+        }
+    }
+
+    public function onAuthenticationFailure($event): void
+    {
+        $this->userNoticeManager->setNotice(UserNoticeConstants::TYPE_SECURITY, 'Two factor authentication failure', 'The two factor code used is wrong', $event->getToken()->getUser());
+    }
+
+    public function onAuthenticationSuccess($event): void
+    {
+        $this->userNoticeManager->setNotice(UserNoticeConstants::TYPE_SECURITY, 'Two factor authentication success', 'The two factor code used is accepted', $event->getToken()->getUser());
     }
 
     public function onFormEvent($event): void
